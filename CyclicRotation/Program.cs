39e594f0e66f8bf40236031dfab19600c4a86424
snishimura <sjiro.nishimura@gmail.com>
2018-02-22 23:24:54 +0100

    Fix : Rename CyclicRotation

--- CyclicRotation/Program.cs
+++ CyclicRotation/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-namespace OddOccurrencesInArray
+namespace CyclicRotation
 {
     class MainClass
     {
--- CyclicRotation/Properties/AssemblyInfo.cs
+++ CyclicRotation/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("OddOccurrencesInArray")]
+[assembly: AssemblyTitle("CyclicRotation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
