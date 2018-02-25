0493eedaa30f8368e9e0bde01efd6041baf0ab9c
snishimura <sjiro.nishimura@gmail.com>
2018-02-25 18:35:24 +0100

    Add solution : Triangle (O(n**3))

---
+++ Triangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Triangle
+{
+    /// <summary>
+    /// https://app.codility.com/programmers/lessons/6-sorting/triangle/
+    /// </summary>
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr1 = { 10, 2, 5, 1, 8, 20 };
+            int[] arr2 = { 10, 50, 5, 1 };
+
+            Console.WriteLine(Solution(arr1));
+            Console.WriteLine(Solution(arr2));
+        }
+
+        public static int Solution(int[] A)
+        {
+            for (var i = 0; i < A.Length; i++)
+            {
+                for (var j = i + 1; j < A.Length; j++)
+                {
+                    for (var k = j + 1; k < A.Length; k++)
+                    {
+                        var len = A[i] + A[j] + A[k];
+                        var max = Math.Max(A[i], Math.Max(A[j], A[k]));
+                        var rest = len - max;
+                        if (max < rest)
+                        {
+                            return 1;
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
