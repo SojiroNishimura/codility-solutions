723a7146d28001d5629610ec1e5897ffbfd5eb55
snishimura <sjiro.nishimura@gmail.com>
2018-03-04 19:27:42 +0100

    Add solution : MaxSliceSum

---
+++ MaxSliceSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MaxSliceSum
+{
+    /// <summary>
+    /// https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_slice_sum/
+    /// </summary>
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr1 = { 3, 2, -6, 4, 0 };
+            int[] arr2 = { -10, -3 };
+
+            Console.WriteLine(Solution(arr1));
+            Console.WriteLine(Solution(arr2));
+        }
+
+        public static int Solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            long maxEnd = int.MinValue;
+            long maxSlice = int.MinValue;
+
+            foreach (var val in A)
+            {
+                if (maxEnd <= 0)
+                {
+                    maxEnd = Math.Max(val, maxEnd + val);
+                }
+                else
+                {
+                    maxEnd = Math.Max(0, maxEnd + val);
+                }
+                maxSlice = Math.Max(maxSlice, maxEnd);
+            }
+            return (int) maxSlice;
+        }
+    }
+}
